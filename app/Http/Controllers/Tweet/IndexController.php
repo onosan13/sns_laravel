af13d146f2942fa844a31876b38b18b93d750bfa
onosan13 <shogo.candy@gmail.com>
2024-06-24 17:12:47 +0000

    つぶやきを取得

---
+++ app/Http/Controllers/Tweet/IndexController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Tweet;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Tweet;
+
+class IndexController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {   
+        $tweets = Tweet::all();
+        return view('tweet.index',[
+            'tweets'=>$tweets,
+        ]);
+    }
+}
